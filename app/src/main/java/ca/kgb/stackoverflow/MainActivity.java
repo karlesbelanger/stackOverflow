ab62b446729111c002347ea98d15afae64b37cc0
karlesbelanger <karlesbelanger@gmail.com>
2016-08-17 15:26:06 -0400

    stackoverflow implemented :P

---
+++ app/src/main/java/ca/kgb/stackoverflow/MainActivity.java
@@ -0,0 +1,21 @@
+package ca.kgb.stackoverflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        overflow();
+    }
+    public void overflow(){
+        double something = 1000000000;
+        while(something > -1000000000){
+            something--;
+            overflow();
+        }
+    }
+}
